afa24c67b7898cbd122ff23daf26d5b31f8076fb
Dammy Akinsiku <me@dammy.net>
2017-08-23 12:04:08 +0100

    Initial Commit

---
+++ pascal.py
@@ -0,0 +1,15 @@
+def pascal(n):
+    set = [1]
+    for x in range(n):
+        set.append(0)
+        set.insert(0, 0)
+
+        new_set = []
+        for y in range(len(set) - 1):
+            a = set[y] + set[y + 1]
+            new_set.append(a)
+        
+        set = new_set
+    return set
+    
+print pascal(10)
---
+++ test_pascal.py
@@ -0,0 +1,26 @@
+import unittest
+import pascal
+
+class PascalTest(unittest.TestCase):
+
+    def test_pascal(self):
+        test_1 = pascal.pascal(0)
+        self.assertEqual(test_1, [1])
+
+        test_2 = pascal.pascal(1)
+        self.assertEqual(test_2, [1,1])
+
+        test_3 = pascal.pascal(2)
+        self.assertEqual(test_3, [1,2,1])
+
+        test_4 = pascal.pascal(3)
+        self.assertEqual(test_4, [1,3,3,1])
+
+        test_5 = pascal.pascal(10)
+        self.assertEqual(test_5, [1, 10, 45, 120, 210, 252, 210, 120, 45, 10, 1])
+
+def main():
+    unittest.main()
+
+if __name__ == "__main__":
+    main()
